0601b6fd4c89862cc9a539cebd654daebb68777c
anna-dolma <anna_dolma.hornschild@mailbox.tu-dresden.de>
2026-06-11 19:12:47 +0200

    add definitions + example for Interpretation and Assigment

--- TheologExercises2026/Exercises/FirstOrderLogic.lean
+++ TheologExercises2026/Exercises/FirstOrderLogic.lean
@@ -12,6 +12,8 @@ namespace Term
 def from_list {α : Type u} (idx : Nat) (l : List (Term α)) : Term α :=
 Term.func idx l.length (fun n => l.get n)
 
+def make_constant (idx : Nat) : Term Nat := from_list idx (@List.nil (Term Nat))
+
 def vars : Term α -> List α
 | .var x => [x]
 |.func _ n f => Fin.foldl n (fun l x => l ++ (f x).vars) []
@@ -70,7 +72,7 @@ theorem args_length (idx n : Nat)  (f : Fin n -> Term α) : (func idx n f).args.
     . grind
 
 def toString [ToString α] : Term α -> String
-| var x => s!"x{ToString.toString x}"
+| var x => s!"x{ToString.toString x} "
 | func idx n f => if n == 0 then s!"c{idx}" else (Fin.foldl n (fun s x => s ++ s!"{toString (f x)}" ++ " ") s!"f{idx}(") ++ ")"
 
 instance [ToString α] : ToString (Term α) where
@@ -78,7 +80,7 @@ instance [ToString α] : ToString (Term α) where
 
 end Term
 
-def x0 := Term.var 3
+def x0 := Term.var 0
 def x1 := Term.var 1
 def x2 := Term.var 2
 
@@ -89,12 +91,10 @@ def t1 := Term.from_list 1 l
 #eval! t1--.toString
 #eval t1.vars
 #eval t1.size
-def e : List (Term Nat) := []
-#eval Term.from_list 1 e
-
+#eval Term.make_constant 0
 
 inductive Formula : Type u
-| atom (idx : Nat) (arity : Nat) : (Fin arity -> Term Nat) -> Formula
+| atom (idx arity : Nat) : (Fin arity -> Term Nat) -> Formula
 | not : Formula -> Formula
 | and  : Formula -> Formula -> Formula
 | or : Formula -> Formula -> Formula
@@ -104,6 +104,9 @@ inductive Formula : Type u
 | éxists : Nat -> Formula -> Formula
 
 namespace Formula
+
+def atom_from_list (idx : Nat) (l : List (Term Nat)) : Formula :=
+  .atom idx l.length (fun n => l.get n)
 
 def variables : Formula -> List Nat
 | atom _ n f => Fin.foldl n (fun l x => l ++ (f x).vars) []
@@ -139,4 +142,74 @@ def toString  : Formula -> String
 | .éxists v f => s!"∃x{v}.{toString f}"
 | .for_all v f => s!"∀x{v}.{toString f}"
 
+instance : ToString Formula where
+  toString := Formula.toString
+
 end Formula
+
+def a1 := Formula.atom_from_list 0 [x0, x1, x2]
+def a2 := Formula.atom_from_list 1 [x1, x2]
+def F1 : Formula := .for_all 0 (.and a1 a2)
+
+structure Interpretation (Δ : Type u) where
+  f_Terms (i n : Nat) : (Fin n -> Δ) -> Δ
+  f_Pred (i n : Nat) : (Fin n -> Δ) -> Prop
+
+def Assignment (Δ : Type u) := Nat -> Δ
+
+def Assignment.map_var {Δ : Type u} (Z : Assignment Δ) (x : Nat) (δ : Δ) : Assignment Δ :=
+fun y => if y = x then δ else Z y
+
+variable {Δ : Type u}
+
+namespace Interpretation
+
+def eval_term (I : Interpretation Δ) (Z : Assignment Δ) : Term Nat -> Δ
+| Term.var x => Z x
+| Term.func i n f => I.f_Terms i n (fun x => I.eval_term Z (f x))
+
+def eval_atom (I : Interpretation Δ) (Z : Assignment Δ) (i n : Nat) (f : Fin n -> Term Nat) : Prop :=
+I.f_Pred i n (fun x => I.eval_term Z (f x))
+
+def eval (I : Interpretation Δ) (Z : Assignment Δ) : Formula -> Prop
+| .atom i n f => I.eval_atom Z i n f
+| .not F => ¬(I.eval Z F)
+| .and F G => (I.eval Z F) ∧ (I.eval Z G)
+| .or F G => (I.eval Z F) ∨ (I.eval Z G)
+| .imp F G => (I.eval Z F) → (I.eval Z G)
+| .eq F G => (I.eval Z F) ↔ (I.eval Z G)
+| .for_all x F => ∀ (δ : Δ), I.eval (Z.map_var x δ) F
+| .éxists x F => ∃ (δ : Δ), I.eval (Z.map_var x δ) F
+
+end Interpretation
+
+def φ1 : Formula := .for_all 0 (.for_all 1 (.eq (Formula.atom_from_list 0 [x0, x1]) (Formula.atom_from_list 0 [x0, x1])))
+
+def t_1 : Fin 2 -> Fin 2 := fun n =>
+match n with
+| 0 => 0
+| 1 => 1
+
+def t_2 : Fin 2 -> Fin 2 := fun n => match n with
+| 0 => 1
+| 1 => 0
+
+def t_3 : Fin 2 -> Fin 2 := fun n => match n with
+| 0 => 1
+| 1 => 1
+
+def I1 : Interpretation (Fin 2) where
+f_Terms := fun _ _ _ => 0                --- 0_o
+f_Pred := fun i n => match i,n with
+| 0, 2 => fun t => t = t_1 ∨ t = t_2 ∨ t = t_3
+| _, _ => fun _ => False
+
+def Z1 : Assignment (Fin 2) := fun _ => 0
+
+theorem test : I1.eval Z1 φ1 := by
+  unfold Interpretation.eval φ1
+  simp only [Fin.forall_fin_two, Fin.isValue]
+  unfold Interpretation.eval Assignment.map_var Formula.atom_from_list
+  simp only [Fin.isValue, List.length_cons, List.length_nil, Nat.reduceAdd, List.get_eq_getElem, Fin.forall_fin_two]
+  unfold Interpretation.eval
+  grind
